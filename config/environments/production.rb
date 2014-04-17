dcbc675c18ea3572d4ad75f6550f9c22a602a961
Gerard <gerard@nerubia.com>
2014-04-17 16:03:25 +0800

    add production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,17 @@ Wikiapp::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'wikiapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'contribute.worldstartupwiki.org' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:               'smtp.mandrillapp.com',
+    port:                  587,
+    domain:                'worldstartupreport.com',
+    user_name:             'bowei+mandrill@worldstartupreport.com',
+    password:              'Z8XdB2DIZBM5SDr2FVthQg',
+    authentication:        :plain
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
