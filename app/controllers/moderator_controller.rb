c3bdcc36ccab2d64e99ceaf2717b725eb181f5b0
Gerard <gerard@nerubia.com>
2014-04-23 17:16:19 +0800

    UI updates and fixes

--- app/controllers/moderator_controller.rb
+++ app/controllers/moderator_controller.rb
@@ -3,7 +3,7 @@ class ModeratorController < ApplicationController
   def index
     if current_user.present? and current_user.ambassador?
       if current_user.countries.eql?('all')
-        @submissions = Submission.includes(:user).where(status: ["pending", "imported"]).limit(10).orderby('id desc')
+        @submissions = Submission.includes(:user).where(status: ["pending", "imported"]).limit(10).order('id desc')
       else
         countries = current_user.countries.split(",")
         @submissions = Submission.includes(:user).where(country: countries, status: ["pending", "imported"]).limit(10).order('id desc')
